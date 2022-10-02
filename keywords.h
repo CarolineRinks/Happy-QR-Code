88603ead205282701c4ac447048369b3b0d15d7f
Caroline Rinks <crinks@vols.utk.edu>
2022-10-02 12:29:40 -0400

    adding initial keywords file

---
+++ keywords.h
@@ -0,0 +1,15 @@
+/* Caroline Rinks
+ * CS494 Fall 2022
+ * Lab #1 Jgraph
+ *
+ * This file contains common strings that are used as keywords for the jgraph program.
+ */
+
+#ifndef KEYWORDS_H
+
+#include <strings>
+using namespace std;
+
+const string newgraph = "newgraph\n";
+
+#endif
